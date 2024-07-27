0d1c030bb25d86ac5fe91186a248155f4417428a
Tanay <tanaymondal6@gmail.com>
2024-07-27 21:41:25 +0530

    Project Updated

--- app/src/main/java/com/tanay/alliancelogin/MainActivity.kt
+++ app/src/main/java/com/tanay/alliancelogin/MainActivity.kt
@@ -10,34 +10,32 @@ import retrofit2.Response
 
 class MainActivity : Activity() {
 
-    private lateinit var btnLogin: Button
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        loginNow()
+    }
+    private fun loginNow() {
+        val call = RetrofitHelper.getAllService()
+            .login(getString(R.string.user_name), getString(R.string.password), "Login")
+        call.enqueue(object : Callback<String> {
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                Toast.makeText(this@MainActivity, "Login Failed. Please try again.", Toast.LENGTH_SHORT).show()
+                finish()
+            }
 
-        btnLogin = findViewById(R.id.btn_login)
-
-        btnLogin.setOnClickListener {
-            val call = RetrofitHelper.getAllService()
-                .login(getString(R.string.user_name), getString(R.string.password), "Login")
-            call.enqueue(object : Callback<String> {
-                override fun onFailure(call: Call<String>, t: Throwable) {
-                    Toast.makeText(this@MainActivity, "Login Failed.", Toast.LENGTH_SHORT).show()
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                if (response.isSuccessful) {
+                    Toast.makeText(this@MainActivity, "Login Success.", Toast.LENGTH_SHORT)
+                        .show()
+                } else {
+                    Toast.makeText(this@MainActivity, "Login Failed. Please try again.", Toast.LENGTH_SHORT)
+                        .show()
                 }
+                finish()
+            }
 
-                override fun onResponse(call: Call<String>, response: Response<String>) {
-                    if (response.isSuccessful) {
-                        Toast.makeText(this@MainActivity, "Login Success.", Toast.LENGTH_SHORT)
-                            .show()
-                    } else {
-                        Toast.makeText(this@MainActivity, "Login Failed.", Toast.LENGTH_SHORT)
-                            .show()
-                    }
-                }
-
-            })
-        }
-
+        })
     }
 }
+
